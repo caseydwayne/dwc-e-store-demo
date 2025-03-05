4784e2b69d5d7fcc92310defff3f3f4c59504b35
Casey Dwayne <webmaster@caseydwayne.com>
2025-03-05 18:35:02 -0500

    modified title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield( 'title', "Dealer's Warehouse Co. | Consumer eStore | Products" )</title>
+    <title>@yield( 'title', "Dealers Warehouse | Consumer E-Store | Products" )</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     @vite( 'resources/css/app.css' )
     <meta name="csrf-token" content="{{ csrf_token() }}">
