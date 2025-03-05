53d5069aa085d1c40f637ca38432777973b13e5c
Casey Dwayne <webmaster@caseydwayne.com>
2025-03-04 19:47:46 -0500

    added product images, performed cleanup and QOL improvements

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,23 +4,15 @@
 
 <div class="landing-page">
 
-    <!-- <h1>Precision Engineered Solutions</h1> -->
-
     <p>
-        Welcome to the Dealers Warehouse Company consumer direct store. 
-        <!-- Discover our range of high-quality products designed with precision and reliability.  -->
-        <!-- We are committed to providing our customers with innovative solutions that meet the highest standards of performance. -->
-    </p>
-
-    <p>
-        <!-- Explore our categories and find the perfect product for your needs. We are dedicated to excellence and customer satisfaction. -->
+        Welcome to Dealers Warehouse Company's consumer direct store. 
     </p>
 
     <h2 class="mt-6">Featured Products</h2>
 
     <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 my-6">
         @foreach( $featuredProducts as $product )                  
-            <x-product-item :product="$product" />
+            <x-product-card-view :product="$product" />
         @endforeach
     </div>
     
